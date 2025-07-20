Fix radial cutoff calculation.

src/jamun/model/arch/e3conv.py
@@ -29,6 +29,7 @@ class E3Conv(torch.nn.Module):
         residue_code_embedding_dim: int,
         residue_index_embedding_dim: int,
         use_residue_sequence_index: bool,
+        max_radius: float,
         num_atom_types: int = 20,
         max_sequence_length: int = 10,
         num_atom_codes: int = 10,
@@ -96,6 +97,7 @@ class E3Conv(torch.nn.Module):
         self.output_head = output_head_factory(irreps_in=self.irreps_hidden, irreps_out=self.irreps_out)
         self.output_gain = torch.nn.Parameter(torch.tensor(0.0))
         self.reduce = reduce
+        self.max_radius = max_radius
 
     def forward(
         self,
@@ -104,7 +106,7 @@ class E3Conv(torch.nn.Module):
         batch: torch.Tensor,
         num_graphs: int,
         c_noise: torch.Tensor,
-        effective_radial_cutoff: float,
+        c_in: torch.Tensor,
     ) -> torch.Tensor:
         # Extract edge attributes.
         edge_index = topology["edge_index"]
@@ -118,7 +120,7 @@ class E3Conv(torch.nn.Module):
         radial_edge_attr = e3nn.math.soft_one_hot_linspace(
             edge_vec.norm(dim=1),
             0.0,
-            effective_radial_cutoff,
+            (c_in * self.max_radius).squeeze(-1),
             self.radial_edge_attr_dim,
             basis="gaussian",
             cutoff=True,

src/jamun/model/arch/orb.py
@@ -1,7 +1,8 @@
 import torch
 from orb_models.forcefield.angular import SphericalHarmonics
 from orb_models.forcefield.base import AtomGraphs
-from orb_models.forcefield.gns import MoleculeGNS
+# from orb_models.forcefield.gns import MoleculeGNS
+from jamun.model.arch.orb_gns import MoleculeGNS
 from orb_models.forcefield.rbf import BesselBasis
 
 from jamun.model.atom_embedding import AtomEmbeddingWithResidueInformation, SimpleAtomEmbedding
@@ -86,13 +87,14 @@ class MoleculeGNSWrapper(torch.nn.Module):
             ),
             activation=activation,
             mlp_norm="rms_norm",
+            max_radius=max_radius,
         )
         if use_torch_compile:
             self.gns.compile(fullgraph=True, dynamic=True)
 
         # print("bro", self.gns._encoder.num_node_in_features, self.gns._encoder.num_edge_in_features)
 
-    def update_features(self, pos: torch.Tensor, topology: AtomGraphs) -> AtomGraphs:
+    def update_features(self, pos: torch.Tensor, topology: AtomGraphs, c_in: torch.Tensor) -> AtomGraphs:
         topology.positions = pos
         topology.node_features["atomic_numbers_embedding"] = self.atom_embedder(
             topology.node_features["atom_type_index"],
@@ -103,6 +105,8 @@ class MoleculeGNSWrapper(torch.nn.Module):
         topology.edge_features["bond_mask_embedding"] = self.bond_edge_embedder(
             topology.edge_features["bond_mask"].long()
         )
+        unscaled_pos = pos / c_in
+        topology.edge_features["vectors"] = unscaled_pos[topology.senders] - unscaled_pos[topology.receivers]
         return topology
 
     def forward(
@@ -112,8 +116,8 @@ class MoleculeGNSWrapper(torch.nn.Module):
         batch: torch.Tensor,
         num_graphs: int,
         c_noise: torch.Tensor,
-        effective_radial_cutoff: float,
+        c_in: torch.Tensor,
     ) -> torch.Tensor:
-        del batch, num_graphs, c_noise, effective_radial_cutoff
-        topology = self.update_features(pos, topology)
+        del batch, num_graphs, c_noise
+        topology = self.update_features(pos, topology, c_in)
         return self.gns(topology)["pred"]

src/jamun/model/denoiser.py
@@ -208,12 +208,8 @@ class Denoiser(pl.LightningModule):
         """Compute the score function."""
         y, topology, batch, num_graphs = data.pos, data.clone(), data.batch, data.num_graphs
         del topology.batch, topology.num_graphs
-        sigma = torch.as_tensor(sigma).to(y)
+        sigma = torch.as_tensor(sigma, device=y.device, dtype=y.dtype)
         return (self.xhat(y, topology, batch, num_graphs, sigma) - y) / (unsqueeze_trailing(sigma, y.ndim - 1) ** 2)
-
-    def effective_radial_cutoff(self, sigma: float | torch.Tensor) -> torch.Tensor:
-        """Compute the effective radial cutoff for the noise level."""
-        return torch.sqrt((self.max_radius**2) + 6 * (sigma**2))
 
     def xhat_normalized(
         self,
@@ -224,7 +220,7 @@ class Denoiser(pl.LightningModule):
         sigma: float | torch.Tensor,
     ) -> torch.Tensor:
         """Compute the denoised prediction using the normalization factors from JAMUN."""
-        sigma = torch.as_tensor(sigma).to(y)
+        sigma = torch.as_tensor(sigma, device=y.device, dtype=y.dtype)
 
         # Compute the normalization factors.
         with torch.cuda.nvtx.range("normalization_factors"):
@@ -237,8 +233,6 @@ class Denoiser(pl.LightningModule):
                 device=y.device,
             )
 
-        radial_cutoff = self.effective_radial_cutoff(sigma) / c_in
-
         # Adjust dimensions.
         c_in = unsqueeze_trailing(c_in, y.ndim - 1)
         c_skip = unsqueeze_trailing(c_skip, y.ndim - 1)
@@ -247,20 +241,20 @@ class Denoiser(pl.LightningModule):
 
         # Add edges to the graph.
         with torch.cuda.nvtx.range("add_edges"):
-            topology = add_edges(y, topology, batch, radial_cutoff)
+            topology = add_edges(y, topology, batch, radial_cutoff=self.max_radius)
 
         with torch.cuda.nvtx.range("scale_y"):
             y_scaled = y * c_in
 
         if self.pass_topology_as_atom_graphs:
-            topology = to_atom_graphs(y_scaled, topology, batch, num_graphs)
+            topology = to_atom_graphs(topology, batch, num_graphs)
 
         with torch.cuda.nvtx.range("g"):
             g_pred = self.g(
                 pos=y_scaled,
                 topology=topology,
                 c_noise=c_noise,
-                effective_radial_cutoff=radial_cutoff,
+                c_in=c_in,
                 batch=batch,
                 num_graphs=num_graphs,
             )
@@ -305,7 +299,7 @@ class Denoiser(pl.LightningModule):
                 with torch.cuda.nvtx.range("mean_center_x"):
                     x = mean_center_f(x, batch, num_graphs)
 
-            sigma = torch.as_tensor(sigma).to(x)
+            sigma = torch.as_tensor(sigma, device=x.device, dtype=x.dtype)
 
             with torch.cuda.nvtx.range("add_noise"):
                 y = self.add_noise(x, sigma, num_graphs)
@@ -401,9 +395,9 @@ class Denoiser(pl.LightningModule):
         del topology.pos, topology.batch, topology.num_graphs
 
         x, batch, num_graphs = data.pos, data.batch, data.num_graphs
-        with torch.cuda.nvtx.range("rotational_augmentation"):
-            if self.rotational_augmentation:
-                R = e3nn.o3.rand_rotation_matrix(device=self.device, dtype=x.dtype)
+        if self.rotational_augmentation:
+            with torch.cuda.nvtx.range("rotational_augmentation"):
+                R = e3nn.o3.rand_matrix(device=self.device, dtype=x.dtype)
                 x = torch.einsum("ni,ij->nj", x, R.T)
 
         loss, aux = self.noise_and_compute_loss(
@@ -437,7 +431,7 @@ class Denoiser(pl.LightningModule):
 
         x, batch, num_graphs = data.pos, data.batch, data.num_graphs
         if self.rotational_augmentation:
-            R = e3nn.o3.rand_rotation_matrix(device=self.device, dtype=x.dtype)
+            R = e3nn.o3.rand_matrix(device=self.device, dtype=x.dtype)
             x = torch.einsum("ni,ji->nj", x, R)
 
         loss, aux = self.noise_and_compute_loss(

src/jamun/model/energy.py
@@ -184,10 +184,6 @@ class EnergyModel(pl.LightningModule):
             y = -y
         return y
 
-    def effective_radial_cutoff(self, sigma: float | torch.Tensor) -> torch.Tensor:
-        """Compute the effective radial cutoff for the noise level."""
-        return torch.sqrt((self.max_radius**2) + 6 * (sigma**2))
-
     def get_model_predictions(
         self,
         pos: torch.Tensor,
@@ -202,7 +198,7 @@ class EnergyModel(pl.LightningModule):
             with torch.cuda.nvtx.range("mean_center_y"):
                 pos = mean_center_f(pos, batch, num_graphs)
 
-        sigma = torch.as_tensor(sigma).to(pos)
+        sigma = torch.as_tensor(sigma, device=pos.device, dtype=pos.dtype)
 
         # Compute the normalization factors.
         with torch.cuda.nvtx.range("normalization_factors"):
@@ -214,7 +210,6 @@ class EnergyModel(pl.LightningModule):
                 D=pos.shape[-1],
                 device=pos.device,
             )
-        radial_cutoff = self.effective_radial_cutoff(sigma) / c_in
 
         # Adjust dimensions.
         c_in = unsqueeze_trailing(c_in, pos.ndim - 1)
@@ -224,12 +219,12 @@ class EnergyModel(pl.LightningModule):
 
         # Add edges to the graph.
         with torch.cuda.nvtx.range("add_edges"):
-            topology = add_edges(pos, topology, batch, radial_cutoff)
+            topology = add_edges(pos, topology, batch, radial_cutoff=self.max_radius)
 
         if self.pass_topology_as_atom_graphs:
-            topology = to_atom_graphs(pos, topology, batch, num_graphs)
+            topology = to_atom_graphs(topology, batch, num_graphs)
 
-        g = functools.partial(self.g, topology=topology, c_noise=c_noise, effective_radial_cutoff=radial_cutoff)
+        g = functools.partial(self.g, topology=topology, c_noise=c_noise, c_in=c_in)
         h = functools.partial(norm_wrapper, g=g, c_in=c_in, c_skip=c_skip, c_out=c_out)
 
         with torch.cuda.nvtx.range("g"):
@@ -406,7 +401,7 @@ class EnergyModel(pl.LightningModule):
 
         x, batch, num_graphs = data.pos, data.batch, data.num_graphs
         if self.rotational_augmentation:
-            R = e3nn.o3.rand_rotation_matrix(device=self.device, dtype=x.dtype)
+            R = e3nn.o3.rand_matrix(device=self.device, dtype=x.dtype)
             x = torch.einsum("ni,ij->nj", x, R.T)
 
         loss, aux = self.noise_and_compute_loss(
@@ -440,7 +435,7 @@ class EnergyModel(pl.LightningModule):
 
         x, batch, num_graphs = data.pos, data.batch, data.num_graphs
         if self.rotational_augmentation:
-            R = e3nn.o3.rand_rotation_matrix(device=self.device, dtype=x.dtype)
+            R = e3nn.o3.rand_matrix(device=self.device, dtype=x.dtype)
             x = torch.einsum("ni,ji->nj", x, R)
 
         loss, aux = self.noise_and_compute_loss(